b8e8eb00f35727f5df8e81752b85f47dc51f2428
divyanshu2003singh <singh2003divyanshu@gmail.com>
2023-09-20 12:13:03 +0530

    Add files via upload

---
+++ Trip_Tide-main/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const userRouter = require("./Router/userRouter");
+const cors =  require("cors");
+const commonRouter = require("./Router/commonRouter");
+const rideRouter = require("./Router/rideRouter");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000
+
+
+const app =express()
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/user", userRouter);
+app.use("/api/common", commonRouter);
+app.use("/api/ride", rideRouter)
+
+
+
+app.listen(port, ()=>{
+    console.log(`server started at port ${port}`)
+})
